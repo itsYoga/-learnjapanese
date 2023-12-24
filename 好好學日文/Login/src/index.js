3643740dbb82ef6d945aeee9e2d8ba46be34c83d
YuJiaLiang0702 <ch993115@gmail.com>
2023-12-25 05:06:31 +0800

    123

--- 好好學日文/Login/src/index.js
+++ 好好學日文/Login/src/index.js
@@ -16,6 +16,10 @@ app.set('view engine', 'ejs');
 // Serve static files from the "public" directory
 app.use(express.static("public"));
 
+// Assuming your images are in the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 //Login button on the right up corner route
 app.get('/sentence', function(req, res) {
     res.render('sentence'); // Assuming your view is named 'login'
