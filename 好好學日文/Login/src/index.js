c71a490b13eca7f2db77fc507efb6473be33268b
YuJiaLiang0702 <ch993115@gmail.com>
2023-12-25 02:47:34 +0800

    123

--- 好好學日文/Login/src/index.js
+++ 好好學日文/Login/src/index.js
@@ -21,53 +21,43 @@ app.get("/signup", (req, res) => {
 });
 
 //Register User
+// Signup user
 app.post("/signup", async (req, res) => {
-
     const data = {
         name: req.body.username,
         password: req.body.password
-    }
+    };
 
-    //check if the user already exsists in the database
+    // Check if the user already exists in the database
     const existingUser = await collection.findOne({ name: data.name });
     if (existingUser) {
-        res.send("User already exsists. Please choose a different usename.");
-    }
-    else {
-        //hash the password using bcrypt
-        const saltRounds =10;
-        const hashedPassword = await bcrypt.hash(data.password,saltRounds);
-
-        data.password = hashedPassword;
-
-        const userdata = await collection.insertMany(data);
-        console.log(userdata);
-    }
-
-
-});
-
-//login user
-app.post("/login", async (req,res)=>{
-    try{
-        const check = await collection.findOne({name: req.body.username});
-        if(!check){
-            res.send("user name cannot found");
-        }
-
-        //compare the hash password from the database with the plain text
-        const isPasswordMatch = await bcrypt.compare(req.body.password, check.passwrod);
-        if(isPasswordMatch){
-            res.render("home");
-        }
-        else{
-            req.send("wrong passwword");
-        }
-    }
-    catch{
-        res.send("wrong Detais")
+        res.send("User already exists. Please choose a different username.");
+    } else {
+        const userdata = await collection.insertOne(data);
+        console.log("User registered with ID:", userdata.insertedId);
     }
 });
+
+// Login user
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.username });
+        if (!check) {
+            res.send("Username doesn't exist");
+        } else {
+            // Compare the plain text password from the request with the one in the database
+            if (req.body.password === check.password) {
+                res.render("home");
+            } else {
+                res.send("Wrong password");
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        res.send("Wrong Details");
+    }
+});
+
 
 const port = 3000;
 app.listen(port, () => {
